f0c2dc1331e0d4387cc3a533dc569c76de4d1ca6
banusion <banusion69@free.fr>
2017-12-16 14:14:45 +0100

    boilerplate for react

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+render(React.createElement("div", null, "Salut les gens"), document.getElementById('app'));
+//# sourceMappingURL=main.js.map
